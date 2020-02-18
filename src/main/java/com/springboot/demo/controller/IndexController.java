c0bfa0222f6151348a4e80247e85ba22e31c0a3a
Potato <516768101@qq.com>
2020-02-18 23:35:28 +0800

    Thymeleaf引入
    Signed-off-by: zz <516768101@qq.com>

--- src/main/java/com/springboot/demo/controller/IndexController.java
+++ src/main/java/com/springboot/demo/controller/IndexController.java
@@ -1,16 +1,31 @@
 package com.springboot.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.springboot.demo.bean.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-@RestController
+@Controller
+//@RestController
 public class IndexController {
-    @Autowired
-    private BlogProperties blogProperties;
-    @RequestMapping("/")
-    public String index(){
-        return blogProperties.getName()+"--"+blogProperties.getTitle();
-    }
+
+    @RequestMapping("/account")
+    public String index(Model m){
+        List<Account> list = new ArrayList<Account>();
+        list.add(new Account("KangKang", "康康", "e10adc3949ba59abbe56e", "超级管理员", "17777777777"));
+        list.add(new Account("Mike", "麦克", "e10adc3949ba59abbe56e", "管理员", "13444444444"));
+        list.add(new Account("Jane","简","e10adc3949ba59abbe56e","运维人员","18666666666"));
+        list.add(new Account("Maria", "玛利亚", "e10adc3949ba59abbe56e", "清算人员", "19999999999"));
+
+        m.addAttribute("accountList",list);
+    return "account";
+
+
+
+}
+
 }
